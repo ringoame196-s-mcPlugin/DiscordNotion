27181082a0c3d74be085d73230a901c0402a2604
りんご飴196 <kazumac41@gmail.com>
2025-01-06 17:53:20 +0900

    ⬆️  :メッセージ修正

--- src/main/kotlin/com/github/ringoame196_s_mcPlugin/Main.kt
+++ src/main/kotlin/com/github/ringoame196_s_mcPlugin/Main.kt
@@ -18,6 +18,6 @@ class Main : JavaPlugin() {
 
     override fun onDisable() {
         super.onDisable()
-        discordWebHookManager.sendDiscordWebhook(":red_circle: サーバー起動しました", "サーバー", "https://p1.hiclipart.com/preview/896/990/447/minecraft-hd-icon-mac-pc-minecraft-icon-512-png-icon.jpg")
+        discordWebHookManager.sendDiscordWebhook(":red_circle: サーバー停止しました", "サーバー", "https://p1.hiclipart.com/preview/896/990/447/minecraft-hd-icon-mac-pc-minecraft-icon-512-png-icon.jpg")
     }
 }
